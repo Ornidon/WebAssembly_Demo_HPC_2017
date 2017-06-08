a019578939afacfa064e54730966a35211e9735a
galedric <g@ledric.me>
2017-06-08 11:01:16 +0200

    Working WASM

--- src/grayscale.c
+++ src/grayscale.c
@@ -23,56 +23,28 @@
 /* 8x8 Bayer ordered dithering. Inspired by Lee Daniel Crocker's http://www.efg2.com/Lab/Library/ImageProcessing/DHALF.TXT */
 void grayscale_dither_ordered(int width, int height, uint8_t *data, uint8_t *processed_data)
 {
-    //const uint8_t pattern[8][8] = {
-    //    { 0, 32,  8, 40,  2, 34, 10, 42},   /* 8x8 Bayer ordered dithering  */
-    //    {48, 16, 56, 24, 50, 18, 58, 26},   /* pattern.  Each input pixel   */
-    //    {12, 44,  4, 36, 14, 46,  6, 38},   /* is scaled to the 0..63 range */
-    //    {60, 28, 52, 20, 62, 30, 54, 22},   /* before looking in this table */
-    //    { 3, 35, 11, 43,  1, 33,  9, 41},   /* to determine the action.     */
-    //    {51, 19, 59, 27, 49, 17, 57, 25},
-    //    {15, 47,  7, 39, 13, 45,  5, 37},
-    //    {63, 31, 55, 23, 61, 29, 53, 21} };
-
-	const uint8_t pattern_t[8][8] = {
-			{ 0, 48, 12, 60,  3, 51, 15, 63},   /* 8x8 Bayer ordered dithering  */
-			{32, 16, 44, 28, 35, 19, 47, 31},   /* pattern.  Each input pixel   */
-			{ 8, 56,  4, 52, 11, 59,  7, 55},   /* is scaled to the 0..63 range */
-			{40, 24, 36, 20, 43, 27, 39, 29},   /* before looking in this table */
-			{ 2, 50, 14, 43,  1, 49, 13, 61},   /* to determine the action.     */
-			{34, 18, 46, 27, 33, 17, 45, 29},
-			{10, 58,  6, 39,  9, 57,  5, 53},
-			{42, 26, 38, 23, 41, 15, 37, 21} };
-
 	uint8_t pixel;
-	__m128i pixels;
-	__m128i pattern;
-	__m128i white = _mm_set_epi16(255, 255, 255, 255, 255, 255, 255, 255);
+	const uint8_t pattern[8][8] = {
+	    { 0, 32,  8, 40,  2, 34, 10, 42},   /* 8x8 Bayer ordered dithering  */
+	    {48, 16, 56, 24, 50, 18, 58, 26},   /* pattern.  Each input pixel   */
+	    {12, 44,  4, 36, 14, 46,  6, 38},   /* is scaled to the 0..63 range */
+	    {60, 28, 52, 20, 62, 30, 54, 22},   /* before looking in this table */
+	    { 3, 35, 11, 43,  1, 33,  9, 41},   /* to determine the action.     */
+	    {51, 19, 59, 27, 49, 17, 57, 25},
+	    {15, 47,  7, 39, 13, 45,  5, 37},
+	    {63, 31, 55, 23, 61, 29, 53, 21} };
+		
 
-    for (int i = 0; i < height; i++) {
-	    int j = 0;
-        for (; (j + 8) <= width; j += 8) {
-	        pixels = _mm_loadl_epi64((const __m128i*) &data[width*i + j]);
-	        pixels = _mm_unpacklo_epi8(pixels, _mm_setzero_si128()); // Extend to 16 bits
-	        pixels = _mm_srli_epi16(pixels, 2);
+	for (int i = 0; i < height; i++) {
+	    for (int j = 0; j < width; j++) {
+	        pixel = data[width*i + j] >> 2; // scaled to 0..63 for Bayer matrix comparison
 
-	        pattern = _mm_loadl_epi64((const __m128i*) &pattern_t[i & 7]);
-	        pattern = _mm_unpacklo_epi8(pattern, _mm_setzero_si128());
-
-			pixels = _mm_and_si128(_mm_cmpgt_epi16(pixels, pattern), white);
-
-	        __m128i packed = _mm_packus_epi16(pixels, _mm_setzero_si128());
-
-	        *((uint64_t*) &processed_data[width*i + j]) = (uint64_t) _mm_cvtsi128_si64(packed);
-        }
-
-	    for (; j < width; j++) {
-		    pixel = data[width*i + j] >> 2; /* scaled to 0..63 for Bayer matrix comparison */
-		    if (pixel > pattern_t[i & 7][j & 7])
-			    processed_data[width*i + j] = 255;
-		    else
-			    processed_data[width*i + j] = 0;
+	        if (pixel > pattern[j & 7][i & 7])
+	            processed_data[width*i + j] = 255;
+	        else
+	            processed_data[width*i + j] = 0;
 	    }
-    }
+	}
 }
 
 
@@ -93,7 +65,7 @@ int main (int argc, char *argv[]){
         perror(__func__);
         exit(EXIT_FAILURE);
     }
-
+	
 	data = stbi_load(SRC_PATH, &width, &height, &n, 0);
     if (!data) {
         fprintf(stderr, "[%s] stbi_load() returned NULL\n", __func__);
--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
 // from https://gist.github.com/kripken/59c67556dc03bb6d57052fedef1e61ab
 // compile with: emcc main.c -Os -s WASM=1 -s SIDE_MODULE=1 -o main.wasm
 // run with: emrun --port 8080 .
-int doubler(int x) {
-  return 2 * x;
+#include <stdlib.h>
+#include <stdint.h>
+
+/* 8x8 Bayer ordered dithering. Inspired by Lee Daniel Crocker's http://www.efg2.com/Lab/Library/ImageProcessing/DHALF.TXT */
+void grayscale_dither_ordered(int width, int height, uint8_t *data, uint8_t *processed_data)
+{	
+	uint8_t pixel;
+	const uint8_t pattern[8][8] = {
+	    { 0, 32,  8, 40,  2, 34, 10, 42},   /* 8x8 Bayer ordered dithering  */
+	    {48, 16, 56, 24, 50, 18, 58, 26},   /* pattern.  Each input pixel   */
+	    {12, 44,  4, 36, 14, 46,  6, 38},   /* is scaled to the 0..63 range */
+	    {60, 28, 52, 20, 62, 30, 54, 22},   /* before looking in this table */
+	    { 3, 35, 11, 43,  1, 33,  9, 41},   /* to determine the action.     */
+	    {51, 19, 59, 27, 49, 17, 57, 25},
+	    {15, 47,  7, 39, 13, 45,  5, 37},
+	    {63, 31, 55, 23, 61, 29, 53, 21} };
+
+	for (int i = 0; i < height; i++) {
+	    for (int j = 0; j < width; j++) {
+	        pixel = data[width*i + j] >> 2; // scaled to 0..63 for Bayer matrix comparison
+
+	        if (pixel > pattern[j & 7][i & 7])
+	            processed_data[width*i + j] = 255;
+	        else
+	            processed_data[width*i + j] = 0;
+	    }
+	}
 }
